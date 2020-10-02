2fec3bb4eb8689abe0f27af5f3fafc8bb6ed6347
Jennifer <67403842+jd9913@users.noreply.github.com>
2020-10-02 15:26:59 -0700

    updating methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -15,5 +15,47 @@ test('creates an employee object', () => {
 });
 
 
+test('gets employees name as object', ()=>{
+
+const employee = new Employee('Mandy');
+
+expect(employee.getName()).toHaveProperty('name');
+
+
+});
+
+test('gets employees name as object', ()=>{
+
+    const employee = new Employee('Mandy');
+    
+    expect(employee.getName()).toHaveProperty('name');
+    
+    
+    });
+    test('gets employees Id as object', ()=>{
+
+        const employee = new Employee('Mandy');
+        
+        expect(employee.getId()).toHaveProperty('id');
+        
+        
+        });
+
+test('gets employees email as object', ()=>{
+
+    expect(employee.getEmail()).toHaveProperty('email');
+
+})
+
+test('gets employees role as object', ()=>{
+
+    expect(employee.getRole()).toHaveProperty('role');
+
+})
+
+
+
+
+
 //Employee parent class with these properties and methods
 //name, id, email, getName(), getId(), getEmail(), getRole()--returns 'Employee'
